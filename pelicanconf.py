930250318657b91a8515f86e54169b4537033689
Katsuma Ito <katsumai@gmail.com>
2013-06-09 04:58:26 +0900

    Init theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,9 +4,10 @@ from __future__ import unicode_literals
 
 AUTHOR = u'ka2n'
 SITENAME = u'ktmtt'
+SITESUBTITLE = u'シンプルになりました'
 SITEURL = ''
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Asia/Tokyo'
 
 DEFAULT_LANG = u'ja'
 
@@ -16,16 +17,17 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  ()
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = ()
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+THEME="ktmtt"
+
+ARTICLE_URL = '-/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '-/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
